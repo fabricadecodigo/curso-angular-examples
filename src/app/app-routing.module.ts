301aa3e214c98d0764310e1feab8030297909230
Felipe <felipe@fabricadecodigo.com>
2020-12-10 08:50:14 -0300

    rotas filhas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,29 @@ import { EditClientesComponent } from './clientes/pages/edit-clientes/edit-clien
 import { ListaClientesComponent } from './clientes/pages/lista-clientes/lista-clientes.component';
 
 const routes: Routes = [
-  { path: '', component: ListaClientesComponent },
-  { path: 'new', component: EditClientesComponent },
-  { path: 'edit/:id', component: EditClientesComponent }
+  // Exemplo 1
+  {
+    path: '',
+    children: [
+      { path: '', component: ListaClientesComponent },
+      { path: 'new', component: EditClientesComponent },
+      { path: 'edit/:id', component: EditClientesComponent },
+    ],
+  },
+
+  // Exemplo 2
+  {
+    path: 'clientes',
+    children: [
+      { path: '', component: ListaClientesComponent },
+      { path: 'new', component: EditClientesComponent },
+      { path: 'edit/:id', component: EditClientesComponent },
+    ],
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
