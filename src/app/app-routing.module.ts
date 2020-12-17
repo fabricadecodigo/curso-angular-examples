79cede428b5d71a0fedcb9c1a98f34b496fdb70f
Felipe <felipe@fabricadecodigo.com>
2020-12-17 08:04:20 -0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
+import { AuthGuard } from './auth/guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'clientes', loadChildren: () => import('./clientes/clientes.module').then(m => m.ClientesModule) },
-  { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: '**', component: PageNotFoundComponent }
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'clientes',
+    loadChildren: () =>
+      import('./clientes/clientes.module').then((m) => m.ClientesModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [AuthGuard],
+  },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,14 @@
+import { LoginComponent } from './pages/login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
