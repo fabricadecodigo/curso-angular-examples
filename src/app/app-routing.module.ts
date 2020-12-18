d797eb0be416d4f6d099d581c737a34810ab0d88
Felipe <felipe@fabricadecodigo.com>
2020-12-18 07:51:15 -0300

    removendo componentes não utilizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'clientes', loadChildren: () => import('./clientes/clientes.module').then(m => m.ClientesModule) },
-  { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '', loadChildren: () => import('./clientes/clientes.module').then(m => m.ClientesModule) },
   { path: '**', component: PageNotFoundComponent }
 ];
 
